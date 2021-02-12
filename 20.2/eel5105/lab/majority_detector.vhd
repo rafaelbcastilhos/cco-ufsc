9c62683159ca6a5eb14df114c9e68ce9494bfc1d
Rafael B. Castilhos <rafaelbcastilhos@gmail.com>
2021-02-12 20:20:49 -0300

    adicionando arquivos iniciais de eel5105

---
+++ 20.2/eel5105/lab/majority_detector.vhd
@@ -0,0 +1,21 @@
+library IEEE;
+use IEEE.Std_Logic_1164.all;
+
+entity usertop is
+port (SW: in std_logic_vector(17 downto 0);
+      LEDR: out std_logic_vector(17 downto 0)
+      );
+end usertop;
+
+architecture circuito_logico of usertop is
+  signal D,E,F,A,B,C,Y: std_logic;
+begin 
+  A <= SW(0);
+  B <= SW(1);
+  C <= SW(2);
+  LEDR(0) <= Y;
+  Y <= D or E or F;
+  D <= A and B;
+  E <= A and C; 
+  F <= B and C;
+end circuito_logico;
