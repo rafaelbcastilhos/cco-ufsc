c66da7175e8b9e568f432fa78b9d6c6f018afc40
Rafael B. Castilhos <rafaelbcastilhos@gmail.com>
2021-04-14 20:18:02 -0300

    adicionando general spider

---
+++ 20.2/ine5454/projeto/jobs/jobs/spiders/general_spider.py
@@ -0,0 +1,71 @@
+from ..items import JobsItem
+from ..regex import *
+from scrapy.linkextractors import LinkExtractor
+import scrapy
+
+
+class GeneralSpider(scrapy.Spider):
+    name = "general_spider"
+    allowed_domains = []
+
+    custom_settings = {
+        'CLOSESPIDER_ITEMCOUNT': 100
+    }
+
+    def start_requests(self):
+        urls = ["https://www.nsctotal.com.br/noticias/busca-empregos-na-area-de-ti-em-santa-catarina-fique-atento"]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        jobs_links = response.xpath("//div[@class='paywall-content']"
+                                    "//div"
+                                    "//div"
+                                    "//div"
+                                    "//p"
+                                    "//a"
+                                    "/@href").getall()
+
+        print(f"jobs_links {jobs_links}")
+
+        if "https://www.nextshipping.com.br/" in jobs_links:
+            jobs_links.remove("https://www.nextshipping.com.br/")
+        for link in jobs_links[2:-3]:
+            yield response.follow(link, self.parse_link)
+
+    def parse_link(self, response):
+        try:
+            link_extractor_second = LinkExtractor()
+            reponse_link = link_extractor_second.extract_links(response)
+
+            print(f"reponse_link {reponse_link}")
+            for link in reponse_link:
+                print(f"Scrape page {link.url}")
+                url = link.url
+                found = response.follow(url, self.parse_job)
+
+                if found:
+                    yield found
+        except:
+            print("error")
+
+    def parse_job(self, second_response):
+        item = JobsItem()
+
+        html = get_html_from_response(second_response)
+        item["title"] = search_title(html)
+        item["description"] = search_description(html)
+        item["company_name"] = None
+        item["hiring_type"] = search_hiring_type(html)
+        item["hierarchy"] = search_hierarchy(html)
+        item["salary"] = search_salary(html)
+        item["mode"] = search_mode(html)
+        item["url"] = second_response.url
+
+        if item["salary"] is not None or \
+                item["mode"] is not None or \
+                item["hierarchy"] is not None or \
+                item["hiring_type"] is not None:
+            return item
+        else:
+            return None
