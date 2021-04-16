115e48ca69a8d98376bc337d6c2c705ab91b9bb6
Rafael B. Castilhos <rafaelbcastilhos@gmail.com>
2021-04-15 21:08:53 -0300

    atualizando fsm led fade

--- 20.2/eel5105/lab/ledfade_fsm/usertop.vhd
+++ 20.2/eel5105/lab/ledfade_fsm/usertop.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity usertop is port (
-        SW: in std_logic_vector(17 downto 0);
+        SW: in std_logic(0);
         CLK_1Hz: in std_logic;
         LEDR: out std_logic_vector(17 downto 0)
     );
@@ -13,9 +13,9 @@ architecture fsm1arq of usertop is
     signal O: std_logic_vector(2 downto 0);
     component FSM1 is
         port (
-           clock: in std_logic;
-           reset: in std_logic;
-           x: out std_logic_vector(2 downto 0)
+            clock: in std_logic;
+            reset: in std_logic;
+            x: out std_logic_vector(2 downto 0)
         );
     end component;
 begin
@@ -26,5 +26,3 @@ begin
 
     LEDR(2 downto 0) <= O;
 end fsm1arq;
-
-
