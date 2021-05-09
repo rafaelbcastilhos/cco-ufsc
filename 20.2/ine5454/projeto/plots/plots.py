63cf165397083dfec334f516bb667c623f4d1d30
Rafael B. Castilhos <rafaelbcastilhos@gmail.com>
2021-05-09 19:18:08 -0300

    update plots

--- 20.2/ine5454/projeto/plots/plots.py
+++ 20.2/ine5454/projeto/plots/plots.py
@@ -1,6 +1,8 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import statistics
+from jobs.jobs.regex import get_digits
 
 with open('/home/rafael/Documentos/cco-ufsc/20.2/ine5454/projeto/jobs/jobs.json', encoding='utf8') as out:
     jobs = json.load(out)
@@ -13,9 +15,9 @@ def bar_hiring_type():
     }
 
     for job in jobs:
-        type = str(job["hiring_type"])
+        type = job["hiring_type"]
         if type == "PJ" or type == "CLT":
-            hiring_type[type] += + 1
+            hiring_type[type] += 1
 
     y_pos = np.arange(len(hiring_type))
     plt.xticks(y_pos, hiring_type.keys())
@@ -24,4 +26,45 @@ def bar_hiring_type():
     plt.show()
 
 
+def bar_salary_hierarchy():
+    relation = {
+        'Sênior': [],
+        'Pleno': [],
+        'Junior': [],
+        'Estagiário': []
+    }
+
+    mean = []
+
+    for job in jobs:
+        salary = job["salary"]
+        hierarchy = job["hierarchy"]
+        if salary is not None and hierarchy is not None:
+            digits = get_digits(salary)
+            if digits != 0:
+                if "£" in salary:
+                    convert = (digits * 7000) / 12
+                    relation[hierarchy].append(convert)
+                elif "€" in salary:
+                    convert = (digits * 6000) / 12
+                    relation[hierarchy].append(convert)
+                elif "US" in salary or "U$" in salary:
+                    convert = (digits * 5000) / 12
+                    relation[hierarchy].append(convert)
+                elif "R$":
+                    relation[hierarchy].append(digits)
+
+    for k in relation.keys():
+        print(relation.get(k))
+        if len(relation.get(k)) == 0:
+            relation.get(k).append(0)
+        mean.append(statistics.mean(relation.get(k)))
+
+    y_pos = np.arange(len(relation))
+    plt.xticks(y_pos, relation.keys())
+    plt.bar(y_pos, mean)
+    plt.show()
+
+
 bar_hiring_type()
+bar_salary_hierarchy()
