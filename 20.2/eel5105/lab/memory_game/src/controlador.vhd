5d6116a3483055545483022cf668efe5542918fc
Rafael B. Castilhos <rafaelbcastilhos@gmail.com>
2021-05-12 00:10:24 -0300

    update comments state

--- 20.2/eel5105/lab/memory_game/src/controlador.vhd
+++ 20.2/eel5105/lab/memory_game/src/controlador.vhd
@@ -24,6 +24,10 @@ begin
 			begin
 				case EAtual is 
                     when start =>
+                    -- reseta 
+                    -- mostra displays com L(level), J(jogo), t(tempo), b(bonus)
+                    -- leds apagados
+                    -- proximo estado: setup
                         r1 <= '1';
                         e1 <= '1';
                         e2 <= '0';
@@ -33,10 +37,15 @@ begin
                         e6 <= '0';
 						PEstado <= setup;
 					when setup =>
+                    -- ativa registrador do setup
+                    -- le entrada sw(13..0)
+                    -- mostra displays com L(level), J(jogo), t(tempo), b(bonus)
+                    -- leds apagados
+                    -- proximo estado: se clique de enter -> play_fpga
                         r1 <= '0';
                         e1 <= '1';
                         e2 <= '0';
-                        e3 <= '1';
+                        e3 <= '0';
                         e4 <= '0';
                         e5 <= '0';
                         e6 <= '0';
@@ -44,6 +53,10 @@ begin
                             PEstado <= play_fpga;
                         end if;
                     when play_fpga =>
+                    -- leds acessos com a sequencia da fpga
+                    -- com tempo definido no estado anterior sw(9..6)
+                    -- mostra displays com L(level), J(jogo), t(tempo), b(bonus)
+                    -- proximo estado: se end fpga -> play_user
                         r1 <= '0';
                         e1 <= '0';
                         e2 <= '1';
@@ -55,6 +68,10 @@ begin
                             PEstado <= play_user;
                         end if;
                     when play_user =>
+                    -- le tentativa em sw(17..0)
+                    -- mostra displays com L(level), J(jogo), t(tempo), b(bonus)
+                    -- leds apagados
+                    -- proximo estado: se clique de enter -> check. se end time -> result
                         r1 <= '0';
                         e1 <= '0';
                         e2 <= '0';
@@ -68,6 +85,10 @@ begin
                             PEstado <= result;
                         end if;
                     when check =>
+                    -- avalia em quantas posicoes o usuario errou na replicacao
+                    -- mostra displays com L(level), J(jogo), t(tempo), b(bonus)
+                    -- leds apagados
+                    -- proximo estado: se end bonus ou end round -> result. se nao -> next_round
                         r1 <= '0';
                         e1 <= '0';
                         e2 <= '0';
@@ -81,6 +102,10 @@ begin
                             PEstado <= next_round;
                         end if;
                     when next_round =>
+                    -- conta rodada
+                    -- mostra displays com L(level), J(jogo), t(tempo), b(bonus)
+                    -- leds apagados
+                    -- proximo estado: wait
                         r1 <= '0';
                         e1 <= '0';
                         e2 <= '0';
@@ -90,6 +115,10 @@ begin
                         e6 <= '0';
                         PEstado <= wait1;
                     when wait1 =>
+                    -- mostra displays com round= e o valor da rodada
+                    -- leds apagados
+                    -- reset da contagem da fpga e tempo da tentativa do usuario
+                    -- proximo estado: se clique de enter -> play_fpga
                         r1 <= '0';
                         e1 <= '0';
                         e2 <= '0';
@@ -101,6 +130,9 @@ begin
                             PEstado <= play_fpga;
                         end if;
                     when result =>
+                    -- dependendo do vencedor mostra displays com USEr= ou FPgA= e a pontuacao final
+                    -- leds apagados
+                    -- proximo estado: se clique de enter -> start
                         r1 <= '0';
                         e1 <= '0';
                         e2 <= '0';
@@ -110,28 +142,8 @@ begin
                         e6 <= '1';
                         if(enter = '0') then
                             PEstado <= start;
-                        else
-                            PEstado <= result;
                         end if;
                 end case;
     end process;
 
 end fsmcontrolador;
-
-
--- when setup =>
--- r1 <= '0';
--- e1 <= '1';
--- e2 <= '0';
--- e3 <= '1';
--- e4 <= '0';
--- e5 <= '0';
--- e6 <= '0';
--- when wait1 =>
--- r1 <= '0';
--- e1 <= '0';
--- e2 <= '1';
--- e3 <= '1';
--- e4 <= '1';
--- e5 <= '1';
--- e6 <= '0';
