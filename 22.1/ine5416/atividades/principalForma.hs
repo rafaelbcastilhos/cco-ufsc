00ea4143ef3125af351c34690f82973e7a83974e
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2022-05-25 22:18:36 -0300

    remove labfiles

--- 22.1/ine5416/atividades/principalForma.hs
+++
@@ -1,8 +0,0 @@
-import ModuloForma (Forma( Circulo ), Forma( Retangulo ), Forma( Triangulo ), Forma( Trapezio ), area, minhaForma)
-
-main = do
-    print(area (Circulo 6.42))
-    print(area (Retangulo 4 5))
-    print(area (Triangulo 4.6 10))
-    print(area (Trapezio 4.6 5.8 5))
-    print(area minhaForma)
