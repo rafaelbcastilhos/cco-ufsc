52e1356dc9c50fca3a571212abeac6d616e01f61
rafael.castilhos <rafael.castilhos@sparkag.com.br>
2022-10-24 22:10:24 -0300

    atualizando bissecao

--- 22.1/ine5202/bissecao.m
+++ 22.1/ine5202/bissecao.m
@@ -2,15 +2,15 @@ clc
 clear
 a=0;
 b=1;
-fa=10-20*(exp(-0.2 * a)-exp(-0.75 * a))-5;
-fb=10-20*(exp(-0.2 * b)-exp(-0.75 * b))-5;
+fa=tan(a);
+fb=tan(b);
 fxm=1;
 k=0;
 erro=10^-5;
 while (abs(fxm)>erro)
     k=k+1;
-    xm=(a+b)/2    
-    fxm=10-20*(exp(-0.2 * xm)-exp(-0.75 * xm))-5;
+    xm=(a+b)/2
+    fxm=tan(xm);
     if fa*fxm<0
         b=xm;
         fb=fxm;
