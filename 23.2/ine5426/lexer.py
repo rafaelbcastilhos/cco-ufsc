ffc09f9e8c5e35d14fefd36db58b05168b8e1f6a
Rafael Castilhos <rafael.castilhos@sparkag.com.br>
2023-08-18 11:37:24 -0300

    atualizando simple lexer

--- 23.2/ine5426/lexer.py
+++ 23.2/ine5426/lexer.py
@@ -11,27 +11,27 @@ class Lexer:
         
         current_char = self.source_code[self.position]
 
-        if re.match(r'[a-zA-Z]', current_char):
+        if re.match(r'[a-zA-Z]', current_char): # Verifica se atende o primeiro estado do diagrama IDENT
             identifier = current_char
             self.position += 1
 
-            while self.position < len(self.source_code) and re.match(r'[a-zA-Z0-9]', self.source_code[self.position]):
+            while self.position < len(self.source_code) and re.match(r'[a-zA-Z0-9]', self.source_code[self.position]): # Verifica se atende o segundo estado do diagrama IDENT
                 identifier += self.source_code[self.position]
                 self.position += 1
 
             return 'IDENTIFICADOR'
         
-        elif re.match(r'[0-9]', current_char):
+        elif re.match(r'[0-9]', current_char): # Verifica se atende o primeiro estado do diagrama NUM_INT
             integer = current_char
             self.position += 1
 
-            while self.position < len(self.source_code) and re.match(r'[0-9]', self.source_code[self.position]):
+            while self.position < len(self.source_code) and re.match(r'[0-9]', self.source_code[self.position]): # Verifica se continua atendendo o primeiro estado do diagrama NUM_INT
                 integer += self.source_code[self.position]
                 self.position += 1
 
             return 'NUM_INT'
 
-        else:  # Outro
+        else:  # Entra no caso OUTRO dos diagramas
             self.position += 1
             return 'OUTRO'
 
@@ -52,6 +52,6 @@ def main(source_code):
     tokens = tokenize(source_code)
     print(tokens)
 
+# Exemplos de teste
 main("1.1")
 main("void main(){ int a; a = 3 + 2; }")
-
